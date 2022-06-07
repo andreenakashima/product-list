529bab93b1f09fc2b8b3f0705cc5d849aa472756
andreenakashima <andreenakashima@gmail.com>
2022-06-07 17:03:34 -0300

    Adicionado lista de produtos no admin

--- app/Http/Controllers/AdminProductController.php
+++ app/Http/Controllers/AdminProductController.php
@@ -2,16 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class AdminProductController extends Controller
 {
     public function index()
     {
-        return view('admin.products');
+        $products = Product::all();
+        return view('admin.products', compact('products'));
     }
+
     public function edit()
     {
         return view('admin.products_edit');
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function create()
+    {
+        return view('admin.products_create');
+    }
+
+    public function store()
+    {
+
     }
 }
